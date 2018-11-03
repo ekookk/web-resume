286213e224bac37aaca03ed471f69a3cd0cf1bc4
ekookk <43214023+ekookk@users.noreply.github.com>
2018-11-02 23:28:06 -0400

    Add files via upload

---
+++ js/webflow.js
@@ -0,0 +1,8 @@
+
+function mOver(body1){
+	body1.innerHTML="Have a nice day! Since the weather is nice, why don't we go on a picnic in the park?",
+  body1.style.color = "green"
+}
+function mOut(body1){
+	body1.innerHTML=""
+}
