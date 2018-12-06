4f31695e96c19b5e905c80826494fa68ca82c9a8
ekookk <echooliu@Gmail.com>
2018-12-06 16:34:59 -0500

    extrapageadded

--- js/webflow.js
+++ js/webflow.js
@@ -2,10 +2,45 @@
 var myBubble = document.querySelector(".chatbubble");
 
 function mOver(body1){
-	myBubble.innerHTML="Have a nice day! Since the weather is nice, why don't we go on a picnic in the park?",
-  myBubble.style.color = "green"
+	myBubble.innerHTML="Click if you want to know about me!",
+  myBubble.style.color = "red"
+	myBubble.style.backgroundColor = "yellow";
 }
 
 function mOut(body1){
 	myBubble.innerHTML=""
+}
+
+function tz(){
+    window.location.href='body1.html';
+}
+
+window.onload = function(){
+ var star = document.getElementById("star");
+ var star_li = star.getElementsByTagName("li");
+ var star_word = document.getElementById("star_word");
+ var result = document.getElementById("result");
+ var i=0;
+ var j=0;
+ var len = star_li.length;
+ var word = ['1','2','3',"4","5"]
+ for(i=0; i<len; i++){
+ star_li[i].index = i;
+ star_li[i].onmouseover = function(){
+  star_word.style.display = "block";
+  star_word.innerHTML = word[this.index];
+  for(i=0; i<=this.index; i++){
+   star_li[i].className = "act";
+  }
+ }
+ star_li[i].onmouseout = function(){
+  star_word.style.display = "none";
+  for(i=0; i<len; i++){
+   star_li[i].className = "";
+  }
+ }
+ star_li[i].onclick = function(){
+  result.innerHTML = (this.index+1)+"star";
+ }
+ }
 }
